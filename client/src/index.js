14f641a2e269dca43583856ac88034a86d74c3e6
Your Name <you@example.com>
2023-01-13 20:35:16 -0300

    deployd front

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import {Provider} from 'react-redux'
 import store from './redux/store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://backend-videogame-production.up.railway.app/'
+axios.defaults.baseURL = 'https://pivideogame-production.up.railway.app/'
 // axios.defaults.baseURL = 'http://localhost:3001'
 
 
